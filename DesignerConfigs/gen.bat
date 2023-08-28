9d23f6ec443fffa1bd810443444971d01b322cc7
qiufei <765913552@qq.com>
2023-08-28 13:30:19 +0800

    接入luban
    Signed-off-by: qiufei <765913552@qq.com>

---
+++ DesignerConfigs/gen.bat
@@ -0,0 +1,17 @@
+set WORKSPACE=..
+
+set LUBAN_DLL=%WORKSPACE%\Tools\Luban\Luban.dll
+set CONF_ROOT=%WORKSPACE%\DesignerConfigs
+
+dotnet %LUBAN_DLL% ^
+    -t all ^
+    -c cs-bin ^
+    -d bin  ^
+    --schemaPath %CONF_ROOT%\Defines\__root__.xml ^
+    -x inputDataDir=%CONF_ROOT%\Datas ^
+    -x outputCodeDir=%WORKSPACE%/Client/Assets/Gen ^
+    -x outputDataDir=%WORKSPACE%\Client\Assets\GenerateDatas\bytes ^
+    -x pathValidator.rootDir=%WORKSPACE%\Client ^
+	-x l10n.textProviderFile=*@%WORKSPACE%\DesignerConfigs\Datas\l10n\texts.json
+
+pause
