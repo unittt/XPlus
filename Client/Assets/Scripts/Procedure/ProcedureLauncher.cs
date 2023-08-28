aff606a6dcb260ac825facf66abf5e163c30c34a
qiufei <765913552@qq.com>
2023-08-28 14:15:10 +0800

    修复启动报错
    Signed-off-by: qiufei <765913552@qq.com>

---
+++ Client/Assets/Scripts/Procedure/ProcedureLauncher.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HT.Framework;
+
+/// <summary>
+/// 启动流程
+/// </summary>
+public class ProcedureLauncher : ProcedureBase
+{
+    /// <summary>
+    /// 流程初始化
+    /// </summary>
+    public override void OnInit()
+    {
+		base.OnInit();
+    }
+    /// <summary>
+    /// 进入流程
+    /// </summary>
+    /// <param name="lastProcedure">上一个离开的流程</param>
+    public override void OnEnter(ProcedureBase lastProcedure)
+    {
+        base.OnEnter(lastProcedure);
+        
+        //初始化设置
+        //登陆流程
+        //游戏流程
+    }
+    /// <summary>
+    /// 离开流程
+    /// </summary>
+    /// <param name="nextProcedure">下一个进入的流程</param>
+    public override void OnLeave(ProcedureBase nextProcedure)
+    {
+        base.OnLeave(nextProcedure);
+    }
+    /// <summary>
+    /// 流程帧刷新
+    /// </summary>
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+    }
+    /// <summary>
+    /// 流程帧刷新（秒）
+    /// </summary>
+    public override void OnUpdateSecond()
+    {
+        base.OnUpdateSecond();
+    }
+}
