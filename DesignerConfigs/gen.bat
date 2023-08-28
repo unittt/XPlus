3d0883d04e564c7ca0dbf134a070c725beb05c9d
qiufei <765913552@qq.com>
2023-08-28 13:55:38 +0800

    修改luban 路径 Signed-off-by: qiufei <765913552@qq.com>

--- DesignerConfigs/gen.bat
+++ DesignerConfigs/gen.bat
@@ -9,8 +9,8 @@ dotnet %LUBAN_DLL% ^
     -d bin  ^
     --schemaPath %CONF_ROOT%\Defines\__root__.xml ^
     -x inputDataDir=%CONF_ROOT%\Datas ^
-    -x outputCodeDir=%WORKSPACE%/Client/Assets/Gen ^
-    -x outputDataDir=%WORKSPACE%\Client\Assets\GenerateDatas\bytes ^
+    -x outputCodeDir=%WORKSPACE%/Client/Assets/Scripts/Gen ^
+    -x outputDataDir=%WORKSPACE%\Client\Assets\GameRes\GenerateDatas\bytes ^
     -x pathValidator.rootDir=%WORKSPACE%\Client ^
 	-x l10n.textProviderFile=*@%WORKSPACE%\DesignerConfigs\Datas\l10n\texts.json
 
