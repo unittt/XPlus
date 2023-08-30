6d9a63bb76daabe2789534ab9910186122322f52
qiufei <765913552@qq.com>
2023-08-30 23:34:00 +0800

    tcp连接成功 Signed-off-by: qiufei <765913552@qq.com>

--- Client/Assets/Scripts/Procedure/ProcedureLogin.cs
+++ Client/Assets/Scripts/Procedure/ProcedureLogin.cs
@@ -13,5 +13,20 @@ public class ProcedureLogin : ProcedureBase
     {
         base.OnEnter(lastProcedure);
         Main.m_UI.OpenUI<UILogin>();
+        
+       
+        Main.m_Network.BeginConnectServerEvent += (channel) =>
+        {
+            Log.Info(channel.ToString() + " 开始连接服务器......");
+        };
+        Main.m_Network.ConnectServerSuccessEvent += (channel) =>
+        {
+            Log.Info(channel.ToString() + " 连接服务器成功！");
+        };
+        Main.m_Network.ConnectServerFailEvent += (channel) =>
+        {
+            Log.Info(channel.ToString() + " 连接服务器失败！");
+        };
+        Main.m_Network.ConnectServer<ProtocolChannel>();
     }
 }
