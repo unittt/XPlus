d4e244cf26bceb7cc2a1230f890acf944b7de897
qiufei <765913552@qq.com>
2023-09-22 17:51:40 +0800

    Type(类型)：新功能/修补bug/文档/格式/重构/增加测试/新增或修改美术资源/关卡/其他文件 Subject(修改的简短描述)： Body(修改目的与动机的详细文字说明)：

--- Client/Assets/StandardAssets/GridMap2/GridMap2.cs
+++ Client/Assets/StandardAssets/GridMap2/GridMap2.cs
@@ -1,7 +1,9 @@
-using System;
+
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Random = Unity.Mathematics.Random;
 
 public class GridMap2 : EditorWindow
 {
@@ -12,6 +14,9 @@ public class GridMap2 : EditorWindow
         wnd.titleContent = new GUIContent("GridMap2");
     }
 
+    private ListView _listView;
+    private List<int> _tempList;
+    
     public void CreateGUI()
     {
         VisualElement root = rootVisualElement;
@@ -31,9 +36,55 @@ public class GridMap2 : EditorWindow
                 // GridMapGlobalConfig.Instance.EditorScenePath = AbsoluteToRelativePath(path);
             }
         };
+        
+        root.Q<Button>("AddButton").clicked += OnClickAdd;
+        root.Q<Button>("RemoveButton").clicked += OnClickRemove;
+       
+        
+        _tempList = new List<int>();
+
+        _listView = root.Q<ListView>("MapListView");
+       
+        _listView.makeItem = MakeListItem;
+        _listView.bindItem = BindListItem;
+        _listView.onSelectionChange += OnSelectItem;
+        _listView.itemsSource = _tempList;
+        _listView.Rebuild();
+    }
+
+    private void OnClickAdd()
+    {
+        var x = Random.CreateFromIndex(0);
+        _tempList.Add(x.NextInt(1,100));
+        _listView.Rebuild();
+    }
+
+    private void OnClickRemove()
+    {
+        if (_listView.selectedIndex < 0) return;
+        _tempList.RemoveAt(_listView.selectedIndex);
+        _listView.Rebuild();
     }
     
+    private VisualElement MakeListItem()
+    {
+        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/StandardAssets/GridMap2/MapInfoElement.uxml");
+        VisualElement labelFromUXML = visualTree.Instantiate();
+        return labelFromUXML;
+    }
     
+    private void BindListItem(VisualElement arg1, int arg2)
+    {
+      
+    }
+    
+    private void OnSelectItem(IEnumerable<object> obj)
+    {
+      
+       
+    }
+
+
     public struct Mapdata
     {
         public int Id;
