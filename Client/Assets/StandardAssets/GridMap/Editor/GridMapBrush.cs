1fe4e37bf3b3b5ab8a9d087a7b6504e5a7b3bb2d
qiufei <765913552@qq.com>
2023-10-24 18:21:44 +0800

    Type(类型)：新功能/修补bug/文档/格式/重构/增加测试/新增或修改美术资源/关卡/其他文件 Subject(修改的简短描述)： Body(修改目的与动机的详细文字说明)：
    Signed-off-by: qiufei <765913552@qq.com>

--- Client/Assets/StandardAssets/GridMap/Editor/GridMapBrush.cs
+++ Client/Assets/StandardAssets/GridMap/Editor/GridMapBrush.cs
@@ -22,121 +22,192 @@ namespace GridMap
 
        public static GridMapManager GridMapManager;
 
-       private static Vector2Int _mouseDownPoint;
+       private static Vector2 _mouseDownPosition;
        private static bool _isMouseDown;
-
-        public static void Update()
-        {
-      
-            var controlID = GridMapBrushHashCode;
-            var controlEventType = Event.current.GetTypeForControl(controlID);
-
-            
-            // if (controlEventType ==  EventType.MouseDrag)
-            // {
-            //     if ((controlEventType == EventType.MouseDrag && GUIUtility.hotControl != controlID) ||
-            //         (Event.current.button != 0 && Event.current.button != 1))
-            //     {
-            //         return;
-            //     }
-            //     
-            //     // make sure we don't use up reserved combinations
-            //     bool inhibitMouseDown = false;
-            //     if (Application.platform == RuntimePlatform.OSXEditor)
-            //     {
-            //         if (Event.current.command && Event.current.alt)
-            //         { // pan combination on mac
-            //             inhibitMouseDown = true;
-            //         }
-            //     }
-            //     
-            //     if (Event.current.type == EventType.MouseDown && !inhibitMouseDown)
-            //     {
-            //         if (UpdateCursorPosition() && !Event.current.shift)
-            //         {
-            //             GUIUtility.hotControl = controlID;
-            //             PencilDrag();
-            //         }
-            //     }
-            //
-            //     if (Event.current.type == EventType.MouseDrag && GUIUtility.hotControl == controlID)
-            //     {
-            //         UpdateCursorPosition();
-            //         PencilDrag();
-            //     }
-            // }
-            // else if (controlEventType == EventType.MouseUp)
-            // {
-            //     if ((Event.current.button == 0 || Event.current.button == 1) && GUIUtility.hotControl == controlID)
-            //     {
-            //         GUIUtility.hotControl = 0;
-            //         RectangleDragEnd();
-            //
-            //         cursorX0 = cursorX;
-            //         cursorY0 = cursorY;
-            //
-            //         // HandleUtility.Repaint();
-            //     }
-            // }
-            // else if (controlEventType == EventType.MouseMove)
-            // {
-            //     UpdateCursorPosition();
-            //     cursorX0 = cursorX;
-            //     cursorY0 = cursorY;
-            // }
-
-        
-            //鼠标按下 并且在 范围内
-            if (controlEventType == EventType.MouseDown && IsInside(out var point))
-            {
-                _mouseDownPoint = point;
-                _isMouseDown = true;
-            }
-
-            if (Input.GetMouseButtonDown(0))
-            {
-                Debug.Log("鼠标抬起");
-            }
-
-            else if (_isMouseDown && controlEventType == EventType.MouseUp)
-            {
-                _isMouseDown = false;
-                
-                var worldRay = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
-                // GridMapManager.GetTileIndexByWorldPos(worldRay.origin, out var x, out var y);
-                IsInside(out var upPoint);
-                Debug.Log(_mouseDownPoint + "     "  +upPoint);
-                if (_mouseDownPoint == upPoint)
-                {
-                    GridMapManager.SetNodeWalkableAndTag(worldRay.origin,(int)Bursh);
-                }
-                else
-                {
-                  
-                    int x0 = Mathf.Min(_mouseDownPoint.x, upPoint.x);
-                    int x1 = Mathf.Max(_mouseDownPoint.x, upPoint.x);
-                    int y0 = Mathf.Min(_mouseDownPoint.y, upPoint.y);
-                    int y1 = Mathf.Max(_mouseDownPoint.y,upPoint.y);
-
-
-                    for (int i = x0; i < x1; i++)
-                    {
-                        for (int j = y0; j < y1; j++)
-                        {
-                            GridMapManager.SetNodeWalkableAndTag(new Vector2(i,j),(int)Bursh);
-                        }
-                    }
-                }
-            }
-          
-
        
-        }
+       
+
+       public static void Update()
+       {
+
+           UpdateMouse();
+       }
+
+       private static void UpdateMouse()
+       {
+            var controlID = GridMapBrushHashCode;
+           var controlEventType = Event.current.GetTypeForControl(controlID);
+           switch (controlEventType)
+           {
+               case EventType.MouseDown:
+               case EventType.MouseDrag:
+                   if ((controlEventType == EventType.MouseDrag && GUIUtility.hotControl != controlID) ||
+                       (Event.current.button != 0 && Event.current.button != 1))
+                   {
+                       return;
+                   }
+
+                   if (controlEventType == EventType.MouseDown)
+                   {
+                       _mouseDownPosition = Event.current.mousePosition;
+                       OnMouseDown(Event.current);
+                   }
+                   
+                   bool inhibitMouseDown = false;
+                   if (Application.platform == RuntimePlatform.OSXEditor)
+                   {
+                       if (Event.current.command && Event.current.alt)
+                       {
+                           // pan combination on mac
+                           inhibitMouseDown = true;
+                       }
+                   }
+
+                   if (Event.current.type == EventType.MouseDown && !inhibitMouseDown)
+                   {
+                       if (!Event.current.shift)
+                       {
+                           GUIUtility.hotControl = controlID;
+                           PencilDrag();
+                         
+                       }
+                   }
+
+                   if (Event.current.type == EventType.MouseDrag && GUIUtility.hotControl == controlID)
+                   {
+                       PencilDrag();
+                       OnMouseDrag(Event.current);
+                   }
+
+                   break;
+
+               case EventType.MouseUp:
+                   if ((Event.current.button == 0 || Event.current.button == 1) && GUIUtility.hotControl == controlID)
+                   {
+                       GUIUtility.hotControl = 0;
+                       // RectangleDragEnd();
+
+                       cursorX0 = cursorX;
+                       cursorY0 = cursorY;
+
+
+                       OnMouseUp(Event.current);
+                       HandleUtility.Repaint();
+                   }
+
+                   break;
+
+               case EventType.Layout:
+                   //HandleUtility.AddDefaultControl(controlID);
+                   break;
+
+               case EventType.MouseMove:
+                   // UpdateCursorPosition();
+                   cursorX0 = cursorX;
+                   cursorY0 = cursorY;
+                   break;
+           }
+       }
+       private static void PencilDrag()
+       {
+           pencilDragActive = true;
+       }
+
+
+       private static void OnMouseDown(Event current)
+       {
+           _mouseDownPosition = current.mousePosition;
+       }
+
+       private static void OnMouseDrag(Event current)
+       {
+           var mousePosition = current.mousePosition;
+           var pos0 = GetWorldPosition(_mouseDownPosition);
+           var pos1 = GetWorldPosition(mousePosition);
+
+           Vector3[] v = new Vector3[4];
+           v[0] = new Vector3(pos0.x, pos0.y, 0);
+           v[1] = new Vector3(pos1.x, pos0.y, 0);
+           v[2] = new Vector3(pos1.x, pos1.y, 0);
+           v[3] = new Vector3(pos0.x, pos1.y, 0);
+           Handles.DrawPolyLine(v);
+           Debug.Log("拖拽中");
+       }
+
+       private static Vector2 GetWorldPosition(Vector2 mousePosition)
+       {
+           var worldRay = HandleUtility.GUIPointToWorldRay(mousePosition);
+           return worldRay.origin;
+       }
+       
+
+       private static void OnMouseUp(Event current)
+       {
+           var mousePosition = current.mousePosition;
+
+    
+       }
+       
+       
+       // var controlID = GridMapBrushHashCode;
+          //   var current = Event.current;
+          //   var controlEventType = Event.current.GetTypeForControl(controlID);
+          //
+          //   switch (controlEventType)
+          //   {
+          //       case EventType.MouseDown:
+          //           if (controlEventType == EventType.MouseDown && IsInside(out var point))
+          //           {
+          //               _mouseDownPoint = point;
+          //               _isMouseDown = true;
+          //               Debug.Log("鼠标按下");
+          //           }
+          //           break;
+          //       
+          //       case EventType.MouseUp:
+          //
+          //           if (_isMouseDown)
+          //           {
+          //               _isMouseDown = false;
+          //
+          //               var worldRay = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+          //               GridMapManager.GetTileIndexByWorldPos(worldRay.origin, out var x, out var y);
+          //               IsInside(out var upPoint);
+          //               Debug.Log(_mouseDownPoint + "     " + upPoint);
+          //               if (_mouseDownPoint == upPoint)
+          //               {
+          //                   GridMapManager.SetNodeWalkableAndTag(worldRay.origin, (int)Bursh);
+          //               }
+          //               else
+          //               {
+          //                   int x0 = Mathf.Min(_mouseDownPoint.x, upPoint.x);
+          //                   int x1 = Mathf.Max(_mouseDownPoint.x, upPoint.x);
+          //                   int y0 = Mathf.Min(_mouseDownPoint.y, upPoint.y);
+          //                   int y1 = Mathf.Max(_mouseDownPoint.y, upPoint.y);
+          //
+          //
+          //                   for (int i = x0; i < x1; i++)
+          //                   {
+          //                       for (int j = y0; j < y1; j++)
+          //                       {
+          //                           GridMapManager.SetNodeWalkableAndTag(new Vector2(i, j), (int)Bursh);
+          //                       }
+          //                   }
+          //               }
+          //           }
+          //
+          //
+          //           // current.Use();
+          //           break;
+          //
+          //       case EventType.Layout:
+          //           HandleUtility.AddDefaultControl(controlID);
+          //           break;
+          //   }
         
-        static void PencilDrag()
-        {
-            pencilDragActive = true;
-        }
+        
+        
+     
        static  void RectangleDragEnd()
     {
         if (!pencilDragActive)
