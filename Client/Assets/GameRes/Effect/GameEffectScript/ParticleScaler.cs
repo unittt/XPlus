072ad72baac97a10e9ec5cf031612e98a0777839
qiufei <765913552@qq.com>
2023-11-29 17:16:45 +0800

    新增shader
    Signed-off-by: qiufei <765913552@qq.com>

--- Client/Assets/GameRes/Effect/GameEffectScript/ParticleScaler.cs
+++ Client/Assets/GameRes/Effect/GameEffectScript/ParticleScaler.cs
@@ -1,20 +1,15 @@
-﻿//This script will only work in editor mode. You cannot adjust the scale dynamically in-game!
+﻿using UnityEngine;
 
-using System;
-using UnityEngine;
 #if UNITY_EDITOR
 using UnityEditor;
-
 #endif
 
 //[ExecuteInEditMode]
 public class ParticleScaler : MonoBehaviour
 {
-    // private ParticleAnimator[] _particleAnimators;
-    // private ParticleEmitter[] _particleEmitters;
-
     private ParticleSystem[] _particleSystems;
     private TrailRenderer[] _trailRenderers;
+    
     public float particleScale = 1.0f;
     public bool alsoScaleGameobject = true;
 
@@ -22,7 +17,6 @@ public class ParticleScaler : MonoBehaviour
 
     private void Start()
     {
-        // init particle system
         if (_particleSystems == null)
             _particleSystems = GetComponentsInChildren<ParticleSystem>(true);
 
@@ -48,21 +42,14 @@ public class ParticleScaler : MonoBehaviour
 
     private void Update()
     {
-        //check if we need to update
-        if (particleScale > 0 && Math.Abs(prevParticleScale - particleScale) > 0.01f)
+        if (particleScale > 0 && Mathf.Abs(prevParticleScale - particleScale) > 0.01f)
         {
             if (alsoScaleGameobject)
                 transform.localScale = new Vector3(particleScale, particleScale, particleScale);
 
             float scaleFactor = particleScale / prevParticleScale;
 
-            //scale legacy particle systems
-            ScaleLegacySystems(scaleFactor);
-
-            //scale shuriken particle systems
             ScaleShurikenSystems(scaleFactor);
-
-            //scale trail renders
             ScaleTrailRenderers(scaleFactor);
 
             prevParticleScale = particleScale;
@@ -71,98 +58,31 @@ public class ParticleScaler : MonoBehaviour
 
     private void ScaleShurikenSystems(float scaleFactor)
     {
-        //get all shuriken systems we need to do scaling on
         if (_particleSystems == null)
             _particleSystems = GetComponentsInChildren<ParticleSystem>(true);
 
         foreach (var ps in _particleSystems)
         {
-            // 缩放粒子属性
-            var mainModule = ps.main;
-            mainModule.startSpeedMultiplier *= scaleFactor;
-            mainModule.startSizeMultiplier *= scaleFactor;
-            mainModule.gravityModifierMultiplier *= scaleFactor;
+            var main = ps.main;
+            main.startSpeedMultiplier *= scaleFactor;
+            main.startSizeMultiplier *= scaleFactor;
+            main.gravityModifierMultiplier *= scaleFactor;
 
-#if UNITY_EDITOR
-            //some variables cannot be accessed through regular script, we will acces them through a serialized object
-            var so = new SerializedObject(ps);
-
-            //unity 4.0 and onwards will already do this one for us
-            so.FindProperty("VelocityModule.x.scalar").floatValue *= scaleFactor;
-            so.FindProperty("VelocityModule.y.scalar").floatValue *= scaleFactor;
-            so.FindProperty("VelocityModule.z.scalar").floatValue *= scaleFactor;
-            so.FindProperty("ClampVelocityModule.magnitude.scalar").floatValue *= scaleFactor;
-            so.FindProperty("ClampVelocityModule.x.scalar").floatValue *= scaleFactor;
-            so.FindProperty("ClampVelocityModule.y.scalar").floatValue *= scaleFactor;
-            so.FindProperty("ClampVelocityModule.z.scalar").floatValue *= scaleFactor;
-            so.FindProperty("ForceModule.x.scalar").floatValue *= scaleFactor;
-            so.FindProperty("ForceModule.y.scalar").floatValue *= scaleFactor;
-            so.FindProperty("ForceModule.z.scalar").floatValue *= scaleFactor;
-            so.FindProperty("ColorBySpeedModule.range").vector2Value *= scaleFactor;
-            so.FindProperty("SizeBySpeedModule.range").vector2Value *= scaleFactor;
-            so.FindProperty("RotationBySpeedModule.range").vector2Value *= scaleFactor;
-
-            so.ApplyModifiedProperties();
-#endif
-        }
-    }
-
-    private void ScaleLegacySystems(float scaleFactor)
-    {
-        //get all emitters we need to do scaling on
-        // if (_particleEmitters == null)
-        //     _particleEmitters = GetComponentsInChildren<ParticleEmitter>(true);
-
-        //apply scaling to emitters
-        foreach (var ps in _particleSystems)
-        {
-            // 缩放粒子属性
-            var mainModule = ps.main;
-            mainModule.startSizeMultiplier *= scaleFactor;
-            mainModule.startSpeedMultiplier *= scaleFactor;
-
-#if UNITY_EDITOR
-            // 仅在Unity编辑器中使用
-            SerializedObject serializedObject = new SerializedObject(ps);
-            SerializedProperty ellipsoidProperty = serializedObject.FindProperty("ShapeModule.m_Ellipsoid");
-            SerializedProperty tangentVelocityProperty = serializedObject.FindProperty("VelocityModule.m_TangentVelocity");
-
-            if (ellipsoidProperty != null)
-            {
-                ellipsoidProperty.vector3Value *= scaleFactor;
-            }
-
-            if (tangentVelocityProperty != null)
-            {
-                tangentVelocityProperty.vector3Value *= scaleFactor;
-            }
-
-            serializedObject.ApplyModifiedProperties();
-#endif
-        }
-
-        foreach (var ps in _particleSystems)
-        {
-            // 获取粒子系统的力模块
-            ParticleSystem.ForceOverLifetimeModule forceModule = ps.forceOverLifetime;
-
-            // 缩放粒子系统的力和随机力
-            forceModule.xMultiplier *= scaleFactor;
-            forceModule.yMultiplier *= scaleFactor;
-            forceModule.zMultiplier *= scaleFactor;
-            forceModule.randomized = true; // 启用随机力
-
-            // 这里你可以根据需要设置其他属性，比如力的曲线等
+            // Scale other modules as needed, example:
+            // var velocity = ps.velocityOverLifetime;
+            // velocity.xMultiplier *= scaleFactor;
+            // velocity.yMultiplier *= scaleFactor;
+            // velocity.zMultiplier *= scaleFactor;
+            
+            // Add additional module scaling here as needed
         }
     }
 
     private void ScaleTrailRenderers(float scaleFactor)
     {
-        //get all animators we need to do scaling on
         if (_trailRenderers == null)
             _trailRenderers = GetComponentsInChildren<TrailRenderer>(true);
 
-        //apply scaling to animators
         foreach (var trail in _trailRenderers)
         {
             trail.startWidth *= scaleFactor;
